a0aa24488b32839705d36d606e8d03dc74253bfb
tjak <tjaktor@gmail.com>
2021-10-25 23:28:07 +0300

    remove unneeded

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 app.use(cors());
 
 const routes = require("./routes/prime");
 app.use(routes);
-
-app.use(express.static("public"));
 
 
 app.use("/", (req, res) => {
