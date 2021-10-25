52053f52338864d41b00d311235f13395e4882b5
tjak <tjaktor@gmail.com>
2021-10-25 22:55:44 +0300

    refactor showprimes

--- client/src/components/ShowPrime.js
+++ client/src/components/ShowPrime.js
@@ -1,25 +1,13 @@
 import React from "react";
 
 export const ShowPrime = (props) => {
-    let primeMessage = "NOT PRIME";
-    
-    if (props.isPrime) {
-        primeMessage = "PRIME!";
-    } else {
-        primeMessage = "NOT PRIME!";
-    }
-
-    return <div>{props.number} is {primeMessage}</div>
+    return <div>{props.number} is {choosePrimeMessage(props.isPrime)}</div>
 }
 
 export const ShowSumPrime = (props) => {
-    let primeMessage = "NOT PRIME";
-    
-    if (props.isPrime) {
-        primeMessage = "PRIME!";
-    } else {
-        primeMessage = "NOT PRIME!";
-    }
+    return <div>{props.numbers} sum is {props.numbersSum} which is {choosePrimeMessage(props.isPrime)}</div>
+}
 
-    return <div>{props.numbers} sum is {props.numbersSum} which is {primeMessage}</div>
+const choosePrimeMessage = (isPrime) => {
+    return isPrime ? "PRIME!" : "NOT PRIME!";
 }
